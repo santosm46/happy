7192171b0b0577993264e73790ada0cd3fc763c7
marcelo <santos46ma@gmail.com>
2020-10-12 20:10:23 -0300

    Arrumado posicionamento do conteúdo da página

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'; // não vai precisar na v17.*
+import './styles/global.css';
+import Routes from './routes';
+
+// componente, uma função retornando jsx
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
